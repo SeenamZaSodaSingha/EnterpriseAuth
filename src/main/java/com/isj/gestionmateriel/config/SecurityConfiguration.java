814a06a9f96e12c98d10c1926e86296f73976374
SeenamZaSodaSingha <thammanit.ch@gmail.com>
2023-09-19 14:08:02 +0700

    CORS Pre-flight FIX

--- src/main/java/com/isj/gestionmateriel/config/SecurityConfiguration.java
+++ src/main/java/com/isj/gestionmateriel/config/SecurityConfiguration.java
@@ -1,26 +1,15 @@
 package com.isj.gestionmateriel.config;
 
-import org.keycloak.adapters.springsecurity.authentication.KeycloakAuthenticationProvider;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.web.servlet.DispatcherServletAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.core.authority.mapping.SimpleAuthorityMapper;
 import org.springframework.security.core.session.SessionRegistry;
 import org.springframework.security.core.session.SessionRegistryImpl;
 import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.authentication.session.NullAuthenticatedSessionStrategy;
 import org.springframework.security.web.authentication.session.RegisterSessionAuthenticationStrategy;
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
-import org.springframework.web.servlet.DispatcherServlet;
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
-
-import jakarta.servlet.Filter;
 import lombok.RequiredArgsConstructor;
 
 @Configuration
@@ -63,7 +52,8 @@ public class SecurityConfiguration  {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http.csrf(csrf -> csrf.disable());
-        http.cors(cors -> cors.disable());
+        // http.cors(cors -> cors.disable());
+        http.cors();
 
         http.authorizeHttpRequests(authorize -> authorize
 
--- src/main/java/com/isj/gestionmateriel/controller/PrivateController.java
+++ src/main/java/com/isj/gestionmateriel/controller/PrivateController.java
@@ -16,7 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/v1")
 @CrossOrigin(origins = "*")
-@Controller
+@RestController
 public class PrivateController {
     
     @RequestMapping(method = RequestMethod.OPTIONS)
